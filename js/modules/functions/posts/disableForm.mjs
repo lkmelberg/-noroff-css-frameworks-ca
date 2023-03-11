6c8bbb36428929ce9cdfdeeaee2511d53416b548
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-03-11 19:33:12 +0100

    updated README

--- js/modules/functions/posts/disableForm.mjs
+++ js/modules/functions/posts/disableForm.mjs
@@ -5,6 +5,8 @@ import {
   addContent,
 } from "../../variables/variables.mjs";
 
+// checks if there is input in the create new post form. Disables/enables button when (no) content is present.
+
 export function success() {
   if (textCreate.value === "" || titleCreate.value === "") {
     postBtn.disabled = true;
