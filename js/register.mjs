f45f8525cc4fd01cc787c0eaa78bab89573638d3
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-03-12 23:04:45 +0100

    fixed register bug

--- js/register.mjs
+++ js/register.mjs
@@ -1,7 +1,4 @@
 import { regForm } from "./modules/variables/variables.mjs";
 import { register } from "./modules/functions/user/create.mjs";
-import { checkIfsignedIn } from "./modules/functions/user/isSignedIn.mjs";
 
 regForm.addEventListener("submit", register);
-
-checkIfsignedIn();
